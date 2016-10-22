210e3b9f00d44aaf7e3122dd46709e5f012daaaf
TajimaSho <b1014258@gmail.com>
2016-10-23 05:31:42 +0900

    [add]シェアテーブルの一覧を出力してくれる機能を追加 [update]QRCode Makerの送信変数を構造体に変更

--- app.js
+++ app.js
@@ -60,9 +60,9 @@ io.sockets.on( 'connection', function( socket ) {
 
   //ShareTable List
   //ShareTableList に新しくテーブルを追加
-  socket.on( 'sharetable_start', function( data ) {
-    data = data + '\n';
-
+  socket.on( 'sharetable_start', function( source ) {
+    data = source.title + "," + source.category + "," + source.endtime + "," + source.explain + "," + source.shopid + "," + source.tableid + "," + source.userid + ":";
+    
     // /csv/ShopList.csv に保存
     fs.appendFile(__dirname + "/csv/ShareTableList.csv", data , 'utf-8', function(err){
       //もし見つからなかったらエラーを返す
@@ -74,6 +74,39 @@ io.sockets.on( 'connection', function( socket ) {
     });
 
     console.log(data);
+  });
+
+  //ShareTableList の一覧を出力
+  socket.on( 'sharetable_list', function() {
+      console.log("test");
+      io.sockets.emit( 'sharetable_list_res', "complete" );
+      io.sockets.emit( 'sharetable_list_back', "tes" );
+    /*
+    // /csv/ShopList.csv に保存
+    fs.readFile(__dirname + "/csv/ShareTableList.csv", 'utf-8', function(err,source){
+      //もし見つからなかったらエラーを返す
+      if(err){
+        io.sockets.emit( 'sharetable_list_res', "error" );
+      }
+      var yenN = source.split(":");
+      var data = new Array();   //dataを配列として宣言
+      for(var i=0;i<yenN.length-1;i++){
+        var tmp = yenN[i].split(",");
+        data[i] = new Object();
+        data[i].title = tmp[0];
+        data[i].category = tmp[1];
+        data[i].endtime = tmp[2];
+        data[i].explain = tmp[3];
+        data[i].shopid = tmp[4];
+        data[i].tableid = tmp[5];
+        data[i].userid = tmp[6];
+      }
+
+      //見つかったらcompleteを返す
+      io.sockets.emit( 'sharetable_list_res', "complete" );
+      io.sockets.emit( 'sharetable_list_back', data );
+    });
+    */
   });
 
   //ShareTableList にテーブルのシェアを終了したことを記録
@@ -89,8 +122,6 @@ io.sockets.on( 'connection', function( socket ) {
       //見つかったらcompleteを返す
       io.sockets.emit( 'sharetable_end_res', "complete" );
     });
-
-    console.log(data);
   });
 
   //Category List
@@ -106,12 +137,15 @@ io.sockets.on( 'connection', function( socket ) {
       //見つかったらcompleteを返す
       //var yenN = source.split('\n');
       var resultString = source.split(",");
+      //var listnum = resultString.length-1;
 
       io.sockets.emit( 'categorylist_res', "complete" );
       io.sockets.emit( 'categorylist_back' , resultString);
+
+      //console.log(listnum);
     });
 
-    //console.log(data);
+    //console.log();
 
   });
 });
